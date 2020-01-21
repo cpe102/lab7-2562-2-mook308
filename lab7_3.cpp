15117a8cf3e0d8b0f9fb3677ef2acd6862bb0727
mook <keettamook_k@cmu.ac.th>
2020-01-21 15:10:38 +0700

    finish 7_3

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -1,1 +1,39 @@
 //status = "ONE-NIGHT-STAND";
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+
+
+
+
+int main()
+{
+  float age,height,asset;
+  string status;
+  cout << "Enter age: ";
+  cin >> age;
+  if(age<20){
+      cout << "Enter height: ";
+      cin >> height;
+      if(height<160)
+      status = "UNFRIEND";
+      else if(height<175)
+      status = "FRIEND";
+      else{
+          cout << "Enter asset: ";
+          cin >> asset;
+          if(asset>69000000){
+            status = "MARRIED";
+          }else{
+            status = "ONE-NIGHT-STAND";
+            }
+        } 
+      cout << "status = " << status;
+
+  }
+  
+
+
+  return 0;
+}
